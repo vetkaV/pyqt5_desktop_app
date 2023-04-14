36a35f773dfbed896df641610b759168745c49e5
nikitaklim1712 <nikitaklim.klim@yandex.ru>
2023-04-14 18:46:30 +0700

    много добавил

--- results.py
+++ results.py
@@ -1,9 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication,QLabel,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit, QWidget
 from instr import *
-from second_win import *
-
-
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -15,27 +12,21 @@ class MainWin(QWidget):
         self.setWindowTitle(txt_title)
         self.resize(win_width,win_height)
         self.move(win_x,win_y)
+
     def initUI(self):
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
+        self.index = QLabel(txt_index)
+        self.workheart = QLabel(txt_workheart)
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.hello_text)
-        self.layout.addWidget(self.instruction)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
-
-
-    def connects(self):
-        self.button.clicked.connect(self.next_click)
-    def next_click(self):
-        self.hide()
-        self.ns = TestWin()
-
+        self.layout.addWidget(self.index,alignment=Qt.AlignCenter)
+        self.layout.addWidget(self.workheart, alignment=Qt.AlignCenter)
         
 
+        self.setLayout(self.layout)
 
-
+        
+    def connects(self):
+        pass
 app = QApplication([])
 mw = MainWin()
 app.exec_()
+
